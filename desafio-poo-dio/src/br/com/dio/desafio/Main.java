78b98f51d6e1aeb88549e916a9de98a3ed3ebfbb
ricardobad51 <rbbelem@gmail.com>
2021-12-16 14:00:09 -0300

    create package dominio e classes Curso e Mentoria

---
+++ desafio-poo-dio/src/br/com/dio/desafio/Main.java
@@ -0,0 +1,31 @@
+package br.com.dio.desafio;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrevendo algo para o curso");
+        curso1.setCargaHoraria(25);
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Descrevendo algo para o curso dois");
+        curso2.setCargaHoraria(15);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Descrição para mentoria de Java");
+        mentoria1.setCargaHoraria(45);
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1 + "\n");
+        System.out.println(curso2 + "\n");
+        System.out.println(mentoria1);
+    }
+}
